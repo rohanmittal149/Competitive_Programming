002e3f56df3f859a3e03dba0f5fb572fffe8609a
Hirdan Aggarwal <aggarwalhirdan@gmail.com>
2019-10-04 14:46:38 +0530

    Update Utopian Tree.py

--- Hackerrank/Utopian Tree.py
+++ Hackerrank/Utopian Tree.py
@@ -3,13 +3,13 @@
 import sys
 
 def utopianTree(n):
-    H = 1
+    h = 1
     i = 1
     while i<=n:
         if i > 0 and i%2 != 0:
-            H = H * 2
+            h = h * 2
         else:
-            H += 1
+            h += 1
         i += 1
     return H
     
